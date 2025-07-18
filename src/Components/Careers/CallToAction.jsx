9025071f7707be4f725e38152a9c474e544cccf1
MABSManvian <mabs.manvian@gmail.com>
2025-07-18 15:36:15 +0530

    Update CallToAction.jsx

--- src/Components/Careers/CallToAction.jsx
+++ src/Components/Careers/CallToAction.jsx
@@ -7,9 +7,11 @@ const CallToAction = () => {
   <p className="text-gray-700 mb-6">
     Discover how MABS can transform your operations with cutting-edge solutions tailored to your needs.
   </p>
-   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px', marginTop: '20px' }}>
+  <Link to="/contact">
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px', marginTop: '20px' }}>
             <button className="ui-btn"><span>Contact Us</span></button>
          </div>
+</Link>
 </div>
 
   )
